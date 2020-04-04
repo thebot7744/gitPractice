cf7c48bcab68f3329dd552aa9e649b0448584ff5
thebot7744 <60073004+thebot7744@users.noreply.github.com>
2020-04-03 21:12:28 -0400

    For flask

--- applicationtest2.py
+++ applicationtest2.py
@@ -1,10 +1,15 @@
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
-def index():
+def homepage():
+    return render_template("index3.html")
+@app.route("/headline")
+
+def index0():
     headline = "Hello, World!!"
     return render_template("index0.html", headline=headline)
 
@@ -13,3 +18,19 @@ def index():
 def bye():
     headline = "Goodbye!"
     return render_template("index0.html", headline=headline)
+
+@app.route("/date")
+
+def index1():
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 5
+    return render_template("index1.html", new_year=new_year)
+
+@app.route("/loop")
+
+def index2():
+    names = ["Alice", "Jim", "Charlotte"]
+    return render_template("index2.html", names=names)
+
+
+
